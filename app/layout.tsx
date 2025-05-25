584f78c682878108158ff8709cc7973d63471b4e
JojoELITE <jojoelite1014@gmail.com>
2025-05-26 00:04:45 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Next.js',
+  title: 'ShopMate',
   description: 'Created with next.js',
   generator: 'next.dev',
 }
